d794c26661765700e1e0f5d7a8ae06d333cde1ea
Dan Savage <daniel.savage@avocet.io>
2021-11-23 18:11:09 +0000

    changed to repo name

--- scripts/integration_test.sh
+++ scripts/integration_test.sh
@@ -2,6 +2,6 @@
 
 echo "===> Start Integration Test"
 docker-compose build #--no-cache
-docker run github_action_libre-server   go test -count=1  -tags=integration -v ./integration_tests/...
+docker run libreoffice-integration-test_libre-server:latest   go test -count=1  -tags=integration -v ./integration_tests/...
 docker-compose down -v --rmi all --remove-oprhans
 docker-compose down -v --rmi all --remove-orphans
