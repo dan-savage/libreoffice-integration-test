5f48621f9126c705a0e785241ccaa0b1cd211974
Dan Savage <daniel.savage@avocet.io>
2021-11-23 10:15:16 +0000

    go project which tests libreoffice

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github_action
+
+go 1.16
+
+require github.com/apex/log v1.9.0
---
+++ main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/csv"
+	"os"
+	"testing"
+
+	"github.com/apex/log"
+)
+
+func TestLibreOfficeConvert(t *testing.T) {
+	LibreOfficeConvert("csv", "test.xls")
+	defer os.Remove("/tmp/test.csv")
+	f, err := os.Open("/tmp/test.csv")
+	if err != nil {
+		log.WithError(err).Error("failed to open tmp csv file")
+		t.Errorf("failed to open file")
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		t.Errorf("failed to read created csv file")
+	}
+
+	o, err := os.Open("./test.csv")
+	if err != nil {
+		t.Errorf("failed to open existing csv file")
+	}
+
+	csvReaderO := csv.NewReader(o)
+	recordsO, err := csvReaderO.ReadAll()
+	if err != nil {
+		t.Errorf("failed to read existing csv file")
+	}
+	if recordsO[0][0] != records[0][0] {
+		t.Errorf("files are not equal")
+	}
+
+}
